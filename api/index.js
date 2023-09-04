c697d186efdcbe9ed58cc5e2de3f23b9516b3018
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2023-09-04 12:33:03 -0300

    Integração Back e front por meio de link

--- api/index.js
+++ api/index.js
@@ -8,10 +8,24 @@ app.use(express.json())//irá fazer o parse de arquivos
 
 app.use('/', express.static('public'))
 
+app.use('/favicon.ico', express.static('public/images/computador.png'))
+
 app.get('/api', (req, res) =>{
     res.status(200).json({
         message: "API fatec funcinando",
         version: "1.0.0"
+    })
+})
+
+
+//Rotas de exceção - devem ser as últimas
+app.use((req, res)=>{
+    res.status(404).json({
+        errors: [{
+            value: `${req.originalUrl}`,
+            msg: `A rota ${req.originalUrl} não existe nesta API!`,
+            param: 'invalid route'
+        }]
     })
 })
 
