63f1b8d0dfa6d39f8b3dc8816b0c5f897f878fe6
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2023-09-18 11:15:26 -0300

    Adicionando mais operações

--- api/routes/prestador.js
+++ api/routes/prestador.js
@@ -1,10 +1,30 @@
 //API REST dos prestadores
 import exepress from 'express'
+import { check, validationResult } from 'express-validator'
 import {connectToDatabase} from '../utils/mongodb.js'
 
 const router =  exepress.Router()
 const {db, ObjectId} = await connectToDatabase()
 const nomeCollection = 'prestadores'
+
+const validaPrestador = [
+    check('cnpj')
+    .not().isEmpty().trim()
+    .withMessage("É obrigatório informar o CNPJ")
+    .isNumeric().withMessage("O CNPJ só deve conter números")
+    .isLength({min: 14, max:14}).withMessage("O CNPJ deve ter 14 números"),
+    check('razao_social')
+    .not().isEmpty().trim()
+    .withMessage("É obrigatório informar a razão social")
+    .isAlphanumeric('pt-BR', {ignore:'/. '})
+    .withMessage("A razão social não deve ter caracteres especiais")
+    .isLength({min: 5}).withMessage("Razão muito curta. Mínimo de 5")
+    .isLength({max: 200}).withMessage("Razão muito longa. Máximo de 200"),
+    check('cnae_fiscal')
+    .isNumeric().withMessage("O código do CNAE deve ser um número"),
+    check('nome_fantasia').optional({nullable: true})
+    
+]
 
 // GET /api/prestadores
 // lista de todos os prestadores
@@ -44,6 +64,32 @@ router.get('/:id', async(req, res)=>{
     }
 })
 
+// DELETE /api/prestadores/:id
+// Apaga o prestador
+router.delete('/:id', async(req, res)=>{
+    await db.collection(nomeCollection)
+    .deleteOne({"_id": {$eq: ObjectId(req.params.id)}})
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(400).json(err))
+})
+
+// DELETE /api/prestadores/:id
+// Apaga o prestador
+router.post('/', validaPrestador, async(req, res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json(({
+            errors: errors.array()
+        }))
+    }else{
+        await db.collection(nomeCollection)
+        .insertOne(req.body)
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).json(err))
+
+    }
+
+})
 
 
 export default router
